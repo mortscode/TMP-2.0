9a17caafeb449fe6767c45b3363cf00b4517421e
Scot Mortimer <scot.mortimer@gmail.com>
2015-09-24 18:00:06 -0700

    post intro kicking ass

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,20 +1,7 @@
 $(document).ready(function() {
 
-  var navHeight = $('.site-header').outerHeight();
-
-  var navpoint = $('.site-header').waypoint(function(direction) {
-
-    if(direction === 'down'){
-      console.log("down");
-      $('.site-header').addClass('is-fixed');
-      $('#content').css('marginTop', navHeight);
-    } else if(direction === 'up'){
-      console.log("up");
-      $('.site-header').removeClass('is-fixed');
-      $('#content').css('marginTop', 0);
-    }
-  }, {
-    offset: 0
+  var sticky = new Waypoint.Sticky({
+    element: $('.site-header')[0]
   });
 
 });
